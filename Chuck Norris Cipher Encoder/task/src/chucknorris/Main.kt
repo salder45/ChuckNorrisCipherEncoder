f39be62fe3828818d514510bac7353f97f3f5739
Eder Martinez <edermtz.04@gmail.com>
2023-01-21 21:42:13 -0600

    Add finish step of the project

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.kt
@@ -1,9 +1,24 @@
 package chucknorris
 
 fun main() {
+    var runAgain: Boolean = true
+    do {
+        println("Please input operation (encode/decode/exit):")
+        val input: String = readln()
+        when (input) {
+            "encode" -> encode()
+            "decode" -> decode()
+            "exit" -> runAgain = false
+            else -> println("There is no '$input' operation")
+        }
+    } while(runAgain)
+    println("Bye!")
+}
+
+fun encode() {
     println("Input string:")
     val input: String = readln()
-    println("The result:")
+    println("Encoded string:")
     var result: String = ""
     var binary: String = ""
     for (inputIndex in input.indices) {
@@ -18,10 +33,10 @@ fun main() {
             else ->  {
                 previousChar = currentChar
                 var firstSequence: String = if (binaryIndex > 0) {
-                        " "
-                    } else {
-                        ""
-                    }
+                    " "
+                } else {
+                    ""
+                }
                 firstSequence+= when (currentChar){
                     '1' -> "0 0"
                     else -> "00 0"
@@ -31,4 +46,51 @@ fun main() {
         }
     }
     println(result)
+}
+
+fun decode() {
+    println("Input encoded string:")
+    val input: String = readln()
+    var notValid: Boolean = false
+    for (chr in input) {
+        if(chr != ' ' && chr != '0') {
+            notValid = true
+        }
+    }
+    val encodedList: List<String> = input.split(" ").toMutableList()
+    if (encodedList.size % 2 != 0) {
+        notValid = true
+    }
+    var binaryResult: String = ""
+    for (index in 0 until encodedList.size / 2) {
+        val binaryBlock: String = encodedList[index * 2]
+        if (binaryBlock != "0" && binaryBlock != "00") {
+            notValid = true
+            break
+        }
+        val digitsBlock: String = encodedList[index* 2 + 1]
+        val binaryValue = if (binaryBlock == "0")  {
+            "1"
+        } else {
+            "0"
+        }
+        binaryResult+= binaryValue.repeat(digitsBlock.length)
+    }
+    if (binaryResult.length % 7 != 0) {
+        notValid = true
+    }
+
+    if(!notValid) {
+        println("Decoded string:")
+        var decodedValue: String = ""
+        for (iteration in 0 until binaryResult.length / 7) {
+            val startingIndex: Int = iteration * 7
+            val binaryValue: String = binaryResult.substring(startingIndex,startingIndex + 7)
+            val decimalValue:  Int =  Integer.parseInt(binaryValue, 2)
+            decodedValue+= decimalValue.toChar()
+        }
+        println(decodedValue)
+    } else {
+        println("Encoded string is not valid.")
+    }
 }

df84900c2e6ba65a10924f844da7e02dcd14fc27
Eder Martinez <edermtz.04@gmail.com>
2023-01-22 21:30:58 -0600

    Formating string

---
+++ Topics/String formatting/Format a table/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a = readln().toDouble()
+    val b = readln().toDouble()
+    val c = readln().toDouble()
+    val d = readln().toDouble()
+
+    // Write your code here
+    println(String.format("%,12.2f%,12.2f\n%,12.2f%,12.2f", a, b, c, d))
+}
